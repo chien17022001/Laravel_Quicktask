b012226d94678fd673f6cf53b644c3a5a30b534a
admin <chiencn05@gmail.com>
2023-07-28 16:52:00 +0700

    chapter4

--- app/Models/User.php
+++ app/Models/User.php
@@ -2,11 +2,16 @@
 
 namespace App\Models;
 
+
+use App\Models\Scopes\AuthScope;
+use Illuminate\Console\View\Components\Task;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
+use Illuminate\Support\Str;
+use Illuminate\Database\Eloquent\Builder;
 
 class User extends Authenticatable
 {
@@ -32,5 +37,28 @@ class User extends Authenticatable
 
     public function tasks(): BelongsToMany{
         return $this->belongsToMany(Task::class);
+    }
+
+
+    public function getFullNameAttribute()
+    {
+        return $this->first_name . ' ' . $this->last_name;
+    }
+
+    public function setUserNameAttribute($value)
+    {
+        $this->attributes['username'] = Str::slug($value, '-');
+    }
+
+
+    public function scopeAdmin(Builder $query): void
+    {
+        $query->where('is_admin', true);
+    }
+
+
+    protected static function booted(): void
+    {
+        static::addGlobalScope(new AuthScope);
     }
 }
