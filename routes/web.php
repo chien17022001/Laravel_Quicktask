edf3fd680fd571dc385f840479f214e7b1c712dd
admin <chiencn05@gmail.com>
2023-07-31 16:36:04 +0700

    chapter6

--- routes/web.php
+++ routes/web.php
@@ -1,18 +1,24 @@
 <?php
 
+use App\Http\Controllers\TaskController;
+use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
-|
-*/
+
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+
+Route::resource('user', UserController::class);
+
+Route::controller(TaskController::class)->group(function(){
+    Route::get('/task','index')->name('task.index');
+    Route::get('/task/create','create')->name('task.index');
+    Route::post('/task','store')->name('task.store');
+    Route::get('/task/{task}','show')->name('task.show');
+    Route::get('/task/{task}/edit','edit')->name('task.edit');
+    Route::put('/task/{task}','update')->name('task.update');
+    Route::delete('/task/{task}','destroy')->name('task.destroy');
 });
