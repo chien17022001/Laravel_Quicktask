3fd83f04a67c7b72b855e36bdfbca39903af3c61
admin <chiencn05@gmail.com>
2023-08-01 15:42:55 +0700

    chapter8

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\TaskController;
 use App\Http\Controllers\UserController;
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
 
@@ -22,3 +23,14 @@ Route::controller(TaskController::class)->group(function(){
     Route::put('/task/{task}','update')->name('task.update');
     Route::delete('/task/{task}','destroy')->name('task.destroy');
 });
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+require __DIR__.'/auth.php';
