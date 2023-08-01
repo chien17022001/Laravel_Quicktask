41f57133b1c370ae1b72a3a20129fa3a0d27784c
admin <chiencn05@gmail.com>
2023-08-01 21:54:41 +0700

    chapter9

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\LanguageController;
 use App\Http\Controllers\TaskController;
 use App\Http\Controllers\UserController;
 use App\Http\Controllers\ProfileController;
@@ -32,5 +33,7 @@ Route::middleware('auth')->group(function () {
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
+
+Route::get('/language/{lang}', [LanguageController::class, 'changePreferredLanguage'])->name('language');
 
 require __DIR__.'/auth.php';
